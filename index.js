5a33f8e54db928a3a3ef5c31fe4424967cbffd7b
irfan-iiitr <imirfansari@gmail.com>
2023-11-29 15:54:11 +0530

    chat room added

--- index.js
+++ index.js
@@ -6,16 +6,20 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const connect = require('./config/db');
-
+const Chat =require('./model/chat');
 
 
 app.use('/',express.static(__dirname + '/src'));
 app.set('view engine', 'ejs');
 
-app.get('/chat/:roomId', (req, res) => {
+app.get('/chat/:roomId', async (req, res) => {
+    const chats=await Chat.find({
+        roomId:req.params.roomId
+    })
     res.render('index',{
         name:'Irfan',
         id:req.params.roomId,
+        chats:chats
     });
 })
 
@@ -23,12 +27,17 @@ io.on('connection', (socket) => {
     console.log('a user connected',socket.id);
 
     socket.on('join_room', (data) => {
-        socket.join(data.roomId);
-        console.log('user joined room',data.roomId);
+        socket.join(data.room_id);
+        console.log('user joined room',data.room_id);
     })
     
-    socket.on('msg_send',(data)=>{  // server recieves a message
+    socket.on('msg_send',async (data)=>{  // server recieves a message
         console.log("message from client",data);
+        const chat = await Chat.create({
+            content:data.msg,
+            user:data.username,
+            roomId:data.room_id
+        })
         io.to(data.roomId).emit('msg_recieved',data); // server sends a message to all other clients
     });
 
