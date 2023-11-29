b4cff9729040b1da74eebc6024f3baf3615a3167
irfan-iiitr <123577873+irfan-iiitr@users.noreply.github.com>
2023-11-29 17:28:54 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI='mongodb+srv://imirfansari:kJGplUJA7wVter4W@cluster0.pcov0c6.mongodb.net/twitter-backend?retryWrites=true&w=majority'
+const mongoURI='mongo uri here...'
 
 const connect = async()=>{
     await mongoose.connect(mongoURI);
